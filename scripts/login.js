69a099ba1354bba0090a1ea4497b32f0d7842a6a
DavidCardonaG <softwarewd@outlook.com>
2021-08-25 22:52:55 -0500

    Agg estructura Js y JSON

---
+++ scripts/login.js
@@ -0,0 +1,24 @@
+let button = document.getElementById('login');
+button.addEventListener('click', function LocalStorage() {
+    let email = document.querySelector('#email').value;
+    let password = document.getElementById('password').value;
+
+    if(email == "" || password == ""){
+        alert('Ingrese todos los campos');
+        return true;
+    }
+    else{
+        if(isNaN()){
+            localStorage.setItem("email", email);
+            localStorage.setItem("password", password);
+            alert('BIENVENIDO, SERAS REDIGERIDO A LA WEB OFICIAL')
+            window.location = "index.html";
+            
+        }else{
+             alert("INCORRECTO");
+        }
+        return false;
+    }
+
+    
+})
